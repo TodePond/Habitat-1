54d3af77739a5fca1e4c2d9f5a8e7d4d67c5c986
Luke Wilson <l2wilson94@gmail.com>
2019-08-17 23:10:17 +0100

    Update Type.js

--- Type.js
+++ Type.js
@@ -20,6 +20,7 @@ Reflect.defineProperty(Object.prototype, "and", {
 	value(type2) {
 		const type1 = this
 		return new Type ({
+			name: `${type1.name} and ${type2.name}`,
 			check: (v) => v.is(type1) && v.is(type2),
 			convert: (v) => v.as(type1).as(type2),
 			depth: Math.max(getTypeDepth(type1), getTypeDepth(type2)) + 1,
@@ -31,6 +32,7 @@ Reflect.defineProperty(Object.prototype, "or", {
 	value(type2) {
 		const type1 = this
 		return new Type ({
+			name: `${type1.name} or ${type2.name}`,
 			check: (v) => v.is(type1) || v.is(type2),
 			convert: (v) => {
 				try   { return v.as(type1).as(type2)}
@@ -45,7 +47,8 @@ Reflect.defineProperty(Object.prototype, "or", {
 })
 
 class Type {
-	constructor({check, convert, depth = 2}) {
+	constructor({name, check, convert, depth = 2}) {
+		this.name = name
 		this.check = check
 		this.convert = convert
 		this.depth = depth
@@ -53,24 +56,29 @@ class Type {
 }
 
 const Any = new Type({
+	name: "Any",
 	check: (a) => true,
 	depth: -1,
 })
 
 const Int = new Type({
+	name: "Int",
 	check: (n) => n % 1 == 0,
 	convert: (n) => parseInt(n),
 })
 
 const Even = new Type({
+	name: "Even",
 	check: (n) => n % 2 == 0,
 })
 
 const Odd = new Type({
+	name: "Odd",
 	check: (n) => n % 2 != 0,
 })
 
 const Positive = new Type({
+	name: "Positive",
 	check: (n) => n >= 0,
 	convert: (n) => Math.abs(n),
 })
@@ -78,24 +86,29 @@ const Positive = new Type({
 const UInt = Int.and(Positive)
 
 const UpperCase = new Type({
+	name: "UpperCase",
 	check: (s) => s == s.as(UpperCase),
 	convert: (s) => s.toUpperCase(),
 })
 
 const LowerCase = new Type({
+	name: "LowerCase",
 	check: (s) => s == s.as(LowerCase),
 	convert: (s) => s.toLowerCase(),
 })
 
 const Capitalised = new Type({
+	name: "Capitalised",
 	check: s => s[0] == s[0].as(UpperCase),
 	convert: s => s.slice(0, 1).as(UpperCase) + s.slice(1),
 })
 
 const ObjectLiteral = new Type({
+	name: "ObjectLiteral",
 	check: o => o.constructor == Object,
 })
 
 const Primitive = new Type({
+	name: "Primitive",
 	check: p => p.is(Number) || p.is(String) || p.is(RegExp) || p.is(Symbol),
 })
