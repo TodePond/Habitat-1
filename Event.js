e896d5fcc041965f79e277cfbd3418d1f03cd1be
Luke Wilson <l2wilson94@gmail.com>
2019-08-17 22:33:18 +0100

    Update Event.js

--- Event.js
+++ Event.js
@@ -1,6 +1,6 @@
-//=======//
-// Event //
-//=======//
+//=========//
+// Element //
+//=========//
 Reflect.defineProperty(Element.prototype, "on", {
 	get() {
 		return new Proxy(this, {
@@ -24,3 +24,57 @@ Reflect.defineProperty(window, "on", {
 		})
 	},
 })
+
+//========//
+// Object //
+//========//
+{
+	
+	Reflect.defineProperty(Object.prototype, "addEventListener", {
+		value(...args) {
+			const eventTarget = summonEventTarget(this)
+			return eventTarget.addEventListener(...args)
+		},
+		writable: true,
+	})
+	
+	Reflect.defineProperty(Object.prototype, "dispatchEvent", {
+		value(...args) {
+			const eventTarget = summonEventTarget(this)
+			return eventTarget.dispatchEvent(...args)
+		},
+		writable: true,
+	})
+	
+	Reflect.defineProperty(Object.prototype, "removeEventListener", {
+		value(...args) {
+			const eventTarget = summonEventTarget(this)
+			return eventTarget.removeEventListener(...args)
+		},
+		writable: true,
+	})
+	
+	Reflect.defineProperty(Object.prototype, "on", {
+		get() {
+			return new Proxy(this, {
+				get: (object, eventName, callback) => (callback) => object.addEventListener(eventName, callback),
+			})
+		},
+	})
+	
+	const EVENT_TARGET_SYMBOL = Symbol("EventTarget")
+	const getEventTarget = (object) => object[EVENT_TARGET_SYMBOL]
+	
+	const createEventTarget = (object) => {
+		const eventTarget = new EventTarget()
+		Reflect.defineProperty(object, EVENT_TARGET_SYMBOL, {value: eventTarget})
+		return eventTarget
+	}
+	
+	const summonEventTarget = (object) => {
+		const eventTarget = getEventTarget(object)
+		if (!eventTarget) return createEventTarget(object)
+		return eventTarget
+	}
+	
+}
